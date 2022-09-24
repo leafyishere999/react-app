10280826a8437f79fb568823d9eb34ab2aee342c
jacobk999 <jababakoshy@gmail.com>
2022-09-24 17:50:17 -0400

    update logo and link

---
+++ commands.sh
@@ -0,0 +1,9 @@
+npx create-react-app my-app
+cd my-app
+git init
+git add .
+git commit -m "first commit"
+git checkout -b main
+git remote add origin https://github.com/leafyishere999/react-app.git
+git push -u origin main
+git checkout -b update_logo
