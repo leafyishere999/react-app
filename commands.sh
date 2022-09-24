da9f76d5214f6566e35cd311c63ba67c131f8f74
jacobk999 <jababakoshy@gmail.com>
2022-09-24 17:57:20 -0400

    add repo link

--- commands.sh
+++ commands.sh
@@ -7,3 +7,7 @@ git checkout -b main
 git remote add origin https://github.com/leafyishere999/react-app.git
 git push -u origin main
 git checkout -b update_logo
+git add .
+git commit -m "update logo and link"
+git push origin update_logo
+# REPO_URL https://github.com/leafyishere999/react-app/
